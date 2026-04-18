7b2bd4d23f279520717eadcaca7cbbda849fe27b
CieL7s <senachanyt@gmail.com>
2026-04-19 05:54:48 +0700

    Create porto.js

---
+++ porto.js
@@ -0,0 +1,15 @@
+[
+    {
+        "id": "01",
+        "title": "Shienime",
+        "type": "Web Application",
+        "description": "Platform streaming anime responsif dengan antarmuka bersih. Fokus pada kecepatan akses dan kemudahan navigasi bagi pengguna.",
+        "tech": [
+            "AnimeJS",
+            "Hono",
+            "JavaScript"
+        ],
+        "link": "https://shienime.eu.cc",
+        "code_preview": "GET /v1/anime/latest\nHost: api.shienime.eu.cc\nStatus: 200 OK"
+    }
+]
